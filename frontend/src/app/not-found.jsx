0fb585648144559d64c6bd408c8aed059c380d22
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-10-28 19:33:20 +0530

    404

--- frontend/src/app/not-found.jsx
+++ frontend/src/app/not-found.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='h-screen bg-gray-200 flex justify-center items-center'>
+      <div className='text-center'>
+        <h1 className='text-9xl font-bold'>404</h1>
+        <p className='text-3xl font-bold'>Page Not Found</p>
+
+        <button className='bg-black rounded-lg px-4 py-2 text-white'>Go to Homepage</button>
+      </div>
+    </div>
   )
 }
 
