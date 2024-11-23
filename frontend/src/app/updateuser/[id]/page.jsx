d275ae2ab34cd6c881f5d87e1fc7d84b2c921064
MehdiiCodes <akbarmehdii2627@gmail.com>
2024-11-23 19:27:38 +0530

    fetchUserData

--- frontend/src/app/updateuser/[id]/page.jsx
+++ frontend/src/app/updateuser/[id]/page.jsx
@@ -1,12 +1,21 @@
 'use client'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const UpdateUser = () => {
 
   const { id } = useParams();
   console.log(id);
+
+
+  const fetchUserData = async () => {
+    const res = await axios.get(`http://localhost:5000/user/getbyid/${id}`);
+    console.log(res.data);
+  }
   
+  useEffect(() => {
+    fetchUserData();
+  }, [])
 
   return (
     <div>UpdateUser</div>
